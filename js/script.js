817275840c32b3311a8db8075c58fe254f1e72d6
Andrew Hinkson <anhinkson8842@hotmail.com>
2020-06-29 12:14:35 -0500

    updated script.js Now at MeetsExp

--- js/script.js
+++ js/script.js
@@ -2,53 +2,54 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
 const list = document.querySelectorAll('ul.student-list li');
+const pageDiv = document.querySelector('div.page');
 
-
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
+/*
+showPage(list, page) uses the page passed in to find the min and max to set the 
+display of the individual people to only show if it is within those values.
+*/
 const showPage = (list, page) => {
+   let min = page * 10 - 10;
+   let max = page * 10;
    for (let i = 0; i < list.length; i++) {
-   
+      if (i >= min && i < max) {
+         list[i].style.display = '';
+      } else {
+         list[i].style.display = 'none';
+      }
    }
 }
 
+/*
+appendPageLinks(list) gets the total list items and uses that to dynamically create
+the pagination links. It then uses and event listener to send showPage the correct "page"
+to show. 
+*/
+const appendPageLinks = (list) => {
+   let numOfPages = list.length / 10;
 
+   let pagination = document.createElement('div');
+   pagination.className = 'pagination';
+   pageDiv.appendChild(pagination);
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
+   let pageList = document.createElement('ul');
+   pagination.appendChild(pageList);
 
+   let x = '';
+   for (let i = 0; i < numOfPages; i++) {
+      x += `<li><a href="#">${i+1}</a></li>`;
+   }
+   
+   pageList.innerHTML = x;
+   showPage(list, 1);
 
+   pageDiv.addEventListener('click', (e) => {
+      if (e.target.tagName == 'A') {
+         showPage(list, e.target.innerText);
+      }
+   });
+}
 
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+appendPageLinks(list);
