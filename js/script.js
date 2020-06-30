3d4c428657e1f9750b24ca75e0a5bad7728dd518
Andrew Hinkson <38588648+hinksonAndrew@users.noreply.github.com>
2020-06-30 15:10:02 -0500

    updated script.js

--- js/script.js
+++ js/script.js
@@ -1,65 +1,90 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
-
 const list = document.querySelectorAll('ul.student-list li');
-const pageDiv = document.querySelector('div.page');
+const perPageMax = 10;
+const pageDivHeader = document.querySelector('div.page-header');
+const searchResults = [];
 
-/*
-showPage(list, page) uses the page passed in to find the min and max to set the 
-display of the individual people to only show if it is within those values.
-*/
+const createSearchInput = () => {
+    const div = document.createElement('div');
+    div.classList.add('student-search');
+    pageDivHeader.appendChild(div);
+ 
+    const input = document.createElement('input');
+    input.placeholder = 'Search for students...';
+ 
+    const button = document.createElement('button');
+    button.innerText = 'Search';
+ 
+    div.append(input, button);
+}
+
+const search = (searchInput, list) => {
+    const h3 = document.querySelectorAll('h3');
+    
+    for (let i = 0; i < list.length; i++) {
+        if (searchInput.value.length !== 0 && 
+            h3[i].textContent.toLowerCase().includes(searchInput.value.toLowerCase())) {
+                list[i].style.display = '';
+                searchResults.push(list[i]);
+        } else {
+            list[i].style.display = 'none';
+            //none found
+        }
+    }
+    appendPageLinks(searchResults);
+}
+
+
 const showPage = (list, page) => {
-   let anchors = document.querySelectorAll('a');
-   let min = page * 10 - 10;
-   let max = page * 10;
-   for (let i = 0; i < list.length; i++) {
-      if (i >= min && i < max) {
-         list[i].style.display = '';
-      } else {
-         list[i].style.display = 'none';
-      }
-   }
-   //added this code to add the active class to the pages
-   for (let i = 0; i < anchors.length; i++) {
-      if (anchors[i].innerText === page) {
-         anchors[i].classList.add('active');
-         console.log('hello');
-      } else {
-         anchors[i].classList.remove('active');
-      }
-   }
+    const start = (page * perPageMax) - perPageMax;
+    const end = page * perPageMax;
+
+    for (let i = 0; i < list.length; i++) {
+        if (i >= start && i < end) {
+            list[i].style.display = '';
+        } else {
+            list[i].style.display = 'none';
+        }
+    }
 }
 
-/*
-appendPageLinks(list) gets the total list items and uses that to dynamically create
-the pagination links. It then uses and event listener to send showPage the correct "page"
-to show. 
-*/
 const appendPageLinks = (list) => {
-   let numOfPages = list.length / 10;
+    const numberOfPages = list.length / perPageMax;
+    const pageDiv = document.querySelector('div.page');
+    const pagination = document.createElement('div');
+    pagination.className = 'pagination';
+    pageDiv.appendChild(pagination);
+    const ul = document.createElement('ul');
+    pagination.appendChild(ul);
+    const searchInput = document.querySelector('input');
+    
+    let pageNav = '';
+    for (let i = 0; i < numberOfPages; i++) {
+        if (i === 0) {
+            pageNav += `<li><a href='#' class='active'>${i+1}</a></li>`
+        } else {
+            pageNav += `<li><a href='#'>${i+1}</a></li>`
+        }
+    }
+    ul.innerHTML = pageNav;
+    const anchor = document.querySelectorAll('ul li a');
+    anchor[0].classList.add('active');
 
-   let pagination = document.createElement('div');
-   pagination.className = 'pagination';
-   pageDiv.appendChild(pagination);
-
-   let pageList = document.createElement('ul');
-   pagination.appendChild(pageList);
-
-   let x = '';
-   for (let i = 0; i < numOfPages; i++) {
-      x += `<li><a href="#">${i+1}</a></li>`;
-   }
-   pageList.innerHTML = x;
-
-   showPage(list, 1);
-
-   pageDiv.addEventListener('click', (e) => {
-      if (e.target.tagName == 'A') {
-         showPage(list, e.target.innerText);
-      }
-   });
+    pageDiv.addEventListener('click', (e) => {
+        if (e.target.tagName === 'A') {
+            for (let i = 0; i < anchor.length; i++) {
+                anchor[i].classList.remove('active');
+            }
+            e.target.classList.add('active');
+            showPage(list, e.target.textContent);
+        } else if (e.target.tagName === 'BUTTON') {
+            pagination.remove();
+            search(searchInput, list);
+            showPage(searchResults, 1);
+            appendPageLinks(list);
+        }
+    });
 }
 
+createSearchInput();
+showPage(list, 1);
 appendPageLinks(list);
