ea55c656b41b35d93b0cba4ba3876444f2c0fc46
Andrew Hinkson <38588648+hinksonAndrew@users.noreply.github.com>
2020-07-01 11:48:17 -0500

    final update for project submission

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 const list = document.querySelectorAll('ul.student-list li');
 const perPageMax = 10;
+let searchResults = [];
 
+/*
+Extra Credit-
+searchList first creates the search bar for the page then takes the
+input.value and searchs against the list that is passed to function.
+If no matches are found then an element is added stating that. 
+Also, I added a keyup listener to search entire list in real time.
+*/
 const searchList = (list) => {
     const pageDivHeader = document.querySelector('div.page-header');
     const div = document.createElement('div');
@@ -14,10 +22,13 @@ const searchList = (list) => {
 
     
     const h3 = document.querySelectorAll('h3');
+    
     pageDivHeader.addEventListener('click', (e) => {
         if (e.target.tagName === 'BUTTON') {
-            document.querySelector('.pagination').remove();
-            let searchResults = [];
+            if (document.querySelector('.pagination') !== null) {
+                document.querySelector('.pagination').remove();
+            }
+            searchResults = [];
             for (let i = 0; i < list.length; i++) {
                 list[i].style.display = 'none';
                 if (h3[i].textContent.includes(input.value)) {
@@ -25,12 +36,57 @@ const searchList = (list) => {
                     searchResults.push(list[i]);
                 }
             }
+            if (searchResults.length !== 0) {
+                if (document.querySelector('.data-not-found') !== null) {
+                    document.querySelector('.data-not-found').remove();
+                }
+                showPage(searchResults, 1);
+                appendPageLinks(searchResults);
+            } else {
+                if (document.querySelector('.data-not-found') == null) {
+                    const notFound = document.createElement('h2');
+                    notFound.textContent = 'Not Found';
+                    notFound.classList.add('data-not-found');
+                    document.querySelector('div.page').appendChild(notFound);
+                }
+            }
+        }    
+    });
+
+    pageDivHeader.addEventListener('keyup', (e) => {
+        if (document.querySelector('.pagination') !== null) {
+            document.querySelector('.pagination').remove();
+        }
+        searchResults = [];
+        for (let i = 0; i < list.length; i++) {
+            list[i].style.display = 'none';
+            if (h3[i].textContent.includes(input.value)) {
+                list[i].style.display = '';
+                searchResults.push(list[i]);
+            }
+        }
+        if (searchResults.length !== 0) {
+            if (document.querySelector('.data-not-found') !== null) {
+                document.querySelector('.data-not-found').remove();
+            }
             showPage(searchResults, 1);
             appendPageLinks(searchResults);
-        }
+        } else {
+            if (document.querySelector('.data-not-found') == null) {
+                const notFound = document.createElement('h2');
+                notFound.textContent = 'No Results';
+                notFound.classList.add('data-not-found');
+                document.querySelector('div.page').appendChild(notFound);
+            }
+        }  
     });
 }
 
+/*
+showPage takes the list passed and the page that it needs to show. 
+It changes the display property for all items and either shows or doesnt
+depending on the index of individual list items.
+*/
 const showPage = (list, page) => {
     const start = (page * perPageMax) - perPageMax;
     const end = page * perPageMax;
@@ -43,6 +99,11 @@ const showPage = (list, page) => {
     }
 }
 
+/*
+appendPageLinks takes a given list and dynamically creates links at bottom 
+of page so that if clicked it will use showPage function and show correct
+page to user. Also highlights current page shown.
+*/
 const appendPageLinks = (list) => {
     const numberOfPages = list.length / perPageMax;
     const pageDiv = document.querySelector('div.page');
@@ -65,20 +126,22 @@ const appendPageLinks = (list) => {
     anchor[0].classList.add('active');
 
     pageDiv.addEventListener('click', (e) => {
-        if (document.querySelector('input').value === '') {
-            if (e.target.tagName === 'A') {
-                for (let i = 0; i < anchor.length; i++) {
-                    anchor[i].classList.remove('active');
-                }
-                e.target.classList.add('active');
+        if (e.target.tagName === 'A') {
+            for (let i = 0; i < list.length; i++) {
+                list[i].style.display = 'none';
+            }
+            for (let i = 0; i < anchor.length; i++) {
+                anchor[i].classList.remove('active');
+            }
+            e.target.classList.add('active');
+            if (document.querySelector('input').value === '') {
                 showPage(list, e.target.textContent);
-            } 
+            } else {
+                showPage(searchResults, e.target.textContent);
+            }
         }
-
     });
 }
-
-
 
 searchList(list);
 showPage(list, 1);
